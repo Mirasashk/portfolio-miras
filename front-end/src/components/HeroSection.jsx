7ab014264bcca3110055f58d1a9568a0b147449e
Miras A <mirasashk@gmail.com>
2024-06-25 16:45:30 +0100

    enabled projects button on hero section

--- front-end/src/components/HeroSection.jsx
+++ front-end/src/components/HeroSection.jsx
@@ -1,38 +1,41 @@
-import React from "react";
-import heroImg from "../assets/hero.jpg";
-import heroVector from "../assets/heroSectionVector.png";
+import heroImg from '../assets/hero.jpg';
+import heroVector from '../assets/heroSectionVector.png';
+import { useNavigate } from 'react-router-dom';
 
 const HeroSection = () => {
+  const navigate = useNavigate();
   return (
-    <div className="w-full h-screen">
+    <div className='w-full h-screen'>
       <img
-        className="top-0 left-0 w-full h-screen object-cover"
+        className='top-0 left-0 w-full h-screen object-cover'
         src={heroImg}
-        alt="hero"
+        alt='hero'
       />
 
-      <div className="absolute lg:top-1/4 top-16 left-0 right-0">
-        <div className="flex justify-center items-center">
-          <div className="grid lg:grid-cols-2 gap-4 w-4/6">
-            <div className="flex pt-4 justify-center items-center w-full">
-              <div className="grid lg:justify-items-start lg:text-left text-center justify-items-center">
-                <p className="text-3xl text-white font-bold ">
+      <div className='absolute lg:top-1/4 top-16 left-0 right-0'>
+        <div className='flex justify-center items-center'>
+          <div className='grid lg:grid-cols-2 gap-4 w-4/6'>
+            <div className='flex pt-4 justify-center items-center w-full'>
+              <div className='grid lg:justify-items-start lg:text-left text-center justify-items-center'>
+                <p className='text-3xl text-white font-bold '>
                   Your Vision, My Code!
                 </p>
-                <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-slate-600 from-10% to-purple-950 to-60% font-bold ">
+                <h1 className='text-4xl text-transparent bg-clip-text bg-gradient-to-r from-slate-600 from-10% to-purple-950 to-60% font-bold '>
                   Hi there, I'm Miras
                 </h1>
-                <p className="text-2xl max-w-96 lg:text-left text-center text-white font-semibold ">
+                <p className='text-2xl max-w-96 lg:text-left text-center text-white font-semibold '>
                   designing and delivering robust web applications using the
                   MERN stack.
                 </p>
-                <button className="mt-2 w-48 hover:bg-teal-600">
+                <button
+                  onClick={() => navigate('/projects')}
+                  className='mt-2 w-48 hover:bg-teal-600'>
                   See projects
                 </button>
               </div>
             </div>
-            <div className="grid lg:mt-0 mt-10 justify-items-center">
-              <img className=" w-96" src={heroVector} alt="" />
+            <div className='grid lg:mt-0 mt-10 justify-items-center'>
+              <img className=' w-96' src={heroVector} alt='' />
             </div>
           </div>
         </div>
